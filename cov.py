ab46e5aaa39a248151f0c10a2158e14b9134d4cb
Alanxyz <alansierra@protonmail.com>
2025-02-24 15:31:50 -0600

    refactor: unused code removed, the covariance matrix is now stored in out/

--- cov.py
+++ cov.py
@@ -1,11 +1,6 @@
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 from utils import *
-from pprint import pprint
 
-
-fits = np.load("cetafits.npy")
 c00 = loadsqmat('jla_v0_covmatrix.dat')
 c11 = loadsqmat('jla_va_covmatrix.dat')
 c22 = loadsqmat('jla_vb_covmatrix.dat')
@@ -30,6 +25,4 @@ for i in range(n):
         c[3 * j, 3 * i + 2] = c02[i, j]
         c[3 * j + 1, 3 * i] = c12[i, j]
 
-plt.imshow(np.log(c/fits), cmap='hot', interpolation='nearest')
-plt.show()
-#np.save("ceta", c)
+np.save("out/cstat", c)
