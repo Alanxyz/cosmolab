e8a2f8f25647d34df634b625c77213a9e11eb18d
Alanxyz <alansierra@protonmail.com>
2025-05-30 21:21:29 -0600

    fix: syntax error

--- desi/healpixdl.sh
+++ desi/healpixdl.sh
@@ -2,9 +2,9 @@
 
 read -r line 
 for n in $line; do
-	[ ! -e dat/skymap-$n.fits ] \
-		&& echo "Downloading hitpix $n"
-		&& wget -P dat/ "https://data.desi.lbl.gov/public/dr1/target/skyhealpixs/v1/skymap-$n.fits.gz" \
-		&& gzip -d dat/skymap-$n.fits.gz
+	[ ! -e dat/skymap-$n.fits ] && \
+		echo "Downloading hitpix $n" && 
+		wget -P dat/ "https://data.desi.lbl.gov/public/dr1/target/skyhealpixs/v1/skymap-$n.fits.gz" && \
+		gzip -d dat/skymap-$n.fits.gz
 done
 
