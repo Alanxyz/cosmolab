be7dd2510355c2f446b4b50891e4f597f22cf42c
Alanxyz <alansierra@protonmail.com>
2025-06-02 19:47:59 -0600

    feat: spectra downloader

---
+++ desi/catalog.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import numpy as np
+
+from astropy.io import fits
+from astropy.table import Table
+from astropy.convolution import convolve, Gaussian1DKernel
+from sparcl.client import SparclClient
+
+table = Table.read('dat/zall-pix-fuji.fits')
+table['HEALPIX']
+mask = table['SPECTYPE'] == 'GALAXY'
+table = table[mask]
+targetid = table['TARGETID']
+x = list(targetid)
+x = [ int(y) for y in x ]
+client = SparclClient()
+
+def getdata(a, b):
+    inc = ['specid', 'flux', 'wavelength', 'redshift', 'subtype']
+    res = client.retrieve_by_specid(specid_list = x[a:b], include = inc, dataset_list = ['DESI-DR1'])
+    return res.data
+
+#print("FASE 1")
+#d = getdata(0, 100_000)
+#np.save('0-100_000', d)
+#print("FASE 2")
+#d = getdata(100_000, 200_000)
+#np.save('100_000-200_000', d)
+#print("FASE 3")
+#d = getdata(200_000, 300_000)
+#np.save('200_000-300_000', d)
+#print("FASE 4")
+#d = getdata(300_000, 400_000)
+#np.save('300_000-400_000', d)
+
