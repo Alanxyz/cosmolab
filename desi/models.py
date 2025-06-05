d0396f65aee83dcceec80a055ea276925e5b8da0
Alanxyz <alansierra@protonmail.com>
2025-06-04 20:25:58 -0600

    refactor: models in a spearete file

---
+++ desi/models.py
@@ -0,0 +1,33 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import (
+    Conv1D, MaxPooling1D, Dense, Flatten, Dropout, BatchNormalization
+)
+
+def buildmodel1(inputlength, numclasses):
+    model = Sequential([
+        Conv1D(32, 7, activation='relu', input_shape=(inputlength, 1)),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Conv1D(64, 5, activation='relu'),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Conv1D(128, 3, activation='relu'),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Flatten(),
+        Dropout(0.3),
+        Dense(64, activation='relu'),
+        Dense(numclasses, activation='softmax')
+    ])
+    model.compile(
+        optimizer=Adam(1e-4),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    return model
+
--- desi/network.py
+++ desi/network.py
@@ -3,12 +3,6 @@ from pprint import pprint
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import (
-    Conv1D, MaxPooling1D, Dense, Flatten, Dropout, BatchNormalization
-)
-from tensorflow.keras.optimizers import Adam
 
 data = []
 sectsize = 50_000
@@ -24,7 +18,8 @@ startypes = ['O', 'B', 'A', 'F', 'G', 'K', 'M']
 
 data = [
     datasect for datasect in data
-    if datasect['subtype'] in startypes]
+    if datasect['subtype'] in startypes
+]
 data = np.array(data)
 
 fluxes = [d['flux'] for d in data]
@@ -48,38 +43,14 @@ xtrain, xtest, ytrain, ytest = train_test_split(
     random_state=42
 )
 
-def buildmodel(inputlength, numclasses):
-    model = Sequential([
-        Conv1D(32, 7, activation='relu', input_shape=(inputlength, 1)),
-        MaxPooling1D(2),
-        BatchNormalization(),
+from models import *
 
-        Conv1D(64, 5, activation='relu'),
-        MaxPooling1D(2),
-        BatchNormalization(),
-
-        Conv1D(128, 3, activation='relu'),
-        MaxPooling1D(2),
-        BatchNormalization(),
-
-        Flatten(),
-        Dropout(0.3),
-        Dense(64, activation='relu'),
-        Dense(numclasses, activation='softmax')
-    ])
-    model.compile(
-        optimizer=Adam(1e-4),
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
-    )
-    return model
-
-model = buildmodel(inputlength=X.shape[1], numclasses=len(le.classes_))
+model = buildmodel1(inputlength=X.shape[1], numclasses=len(le.classes_))
 history = model.fit(
     xtrain,
     ytrain,
     validation_data=(xtest, ytest),
-    epochs=20,
+    epochs=4,
     batch_size=64
 )
 
