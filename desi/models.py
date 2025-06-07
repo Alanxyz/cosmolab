86ae60324f9d22eb050353120e69028c38fd40b9
Alanxyz <alansierra@protonmail.com>
2025-06-06 23:58:15 -0600

    feat: various models

--- desi/models.py
+++ desi/models.py
@@ -31,3 +31,50 @@ def buildmodel1(inputlength, numclasses):
     )
     return model
 
+def buildmodel0(inputlength, numclasses):
+    model = Sequential([
+        Conv1D(32, 3, activation='relu', input_shape=(inputlength, 1)),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Flatten(),
+        Dropout(0.3),
+        Dense(64, activation='relu'),
+        Dense(numclasses, activation='softmax')
+    ])
+    model.compile(
+        optimizer=Adam(1e-4),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    return model
+
+def buildmodel2(inputlength, numclasses):
+    model = Sequential([
+        Conv1D(16, 7, activation='relu', input_shape=(inputlength, 1)),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Conv1D(16, 7, activation='relu', input_shape=(inputlength, 1)),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Conv1D(16, 7, activation='relu', input_shape=(inputlength, 1)),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Conv1D(16, 7, activation='relu', input_shape=(inputlength, 1)),
+        MaxPooling1D(2),
+        BatchNormalization(),
+
+        Flatten(),
+        Dropout(0.3),
+        Dense(64, activation='relu'),
+        Dense(numclasses, activation='softmax')
+    ])
+    model.compile(
+        optimizer=Adam(1e-4),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    return model
